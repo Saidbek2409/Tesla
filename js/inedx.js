3d3563a47fe8a74f3affc3be0073af03f8ff03db
Saidbek <106152417+Saidbek2409@users.noreply.github.com>
2023-04-25 09:28:25 +0500

    Add files via upload

---
+++ js/inedx.js
@@ -0,0 +1,15 @@
+// let doc = document
+
+
+
+// let switchbtn = doc.querySelector('.switch')
+// let img = './img/черный салон .svg'
+// let img2 = './img/Tesla.svg'
+
+// console.log(switchbtn );
+
+
+
+// switchbtn.addEventListener('click', () => {
+//     img.setAttribute('src', img2)
+// })
